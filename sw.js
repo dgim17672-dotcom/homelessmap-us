57e622827a0c5ac5acdf8d9baa867e26871ca1f4
dgim17672-dotcom <dgim17672@gmail.com>
2026-04-25 06:29:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'homelessmap-us-v1';
+const urlsToCache = [
+  '/homelessmap-us/',
+  '/homelessmap-us/index.html',
+  '/homelessmap-us/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
